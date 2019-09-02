4be8b59897392c05d8b0e7e4b164d354287ce1aa
zhongzhaoguo <248686477@qq.com>
2019-09-02 18:33:07 +0800

    loader的使用

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,10 @@
+import avatar from './avatar.png';
+import './index.scss';
+import createAvater from './createAvatar.js';
+
+var img = new Image();
+img.src = avatar;
+img.classList.add('avatar');
+
+var root = document.getElementById('root');
+root.append(img)
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,35 @@
-const path=require('path');
+const path = require('path');
 
-module.exports={
-	entry:'./index.js',
-	output:{
-		filename:'bundle.js',
-		path:path.resolve(__dirname,'bundle')
+module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						name: '[name]_[hash].[ext]', //placeholder 占位符 配置打包后文件名
+						outputPath: 'images/', //配置打包放置文件夹
+						limit: 2048 //文件的大小限制 如果超过 则打包成文件 不超过打包成base64
+					}
+				}
+			}, {
+				test: /\.scss$/,
+				use: [
+					'style-loader', {
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2
+						}
+					}, 'sass-loader', 'postcss-loader'
+				]
+			}
+		]
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'bundle')
 	}
 }
